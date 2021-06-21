46f5eeeefd1c817decc4d116eb340198c8ed6290
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-06-21 19:57:59 -0300

    style: finish desktop style and layout

--- utils/handleMessageSent.js
+++ utils/handleMessageSent.js
@@ -3,8 +3,14 @@ const formatChatMessage = require('./formatChatMessage');
 
 function handleMessageSent({ io, chatMessage, nickname }) {
   const timestamp = moment().format('DD-MM-YYYY HH:mm:ss');
-  const formattedMessage = formatChatMessage({ chatMessage, nickname, timestamp });
-  io.emit('message', formattedMessage);
+  /* const formattedMessage = formatChatMessage({ chatMessage, nickname, timestamp });
+  io.emit('message', formattedMessage); */
+  const message = {
+    timestamp,
+    nickname,
+    chatMessage,
+  };
+  io.emit('message', message);
 }
 
 module.exports = handleMessageSent;
